c11e3753cd356392444440d3d1179192a6a85ecc
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-12-02 18:31:53 +0000

    base files

---
+++ src/bot.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+const { SapphireClient } = require('@sapphire/framework');
+const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+client.login(process.env.DISCORD_TOKEN);
