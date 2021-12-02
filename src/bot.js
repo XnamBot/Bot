0c1ebca1044b5267844252ba4459fcdba8c68291
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-12-02 19:47:17 +0000

    changed the format

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
 const { SapphireClient, Command } = require('@sapphire/framework');
-const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'], defaultPrefix: ['|', 'Xnam!', 'xnam!'] });
-
+const client = new SapphireClient(
+    {
+        intents: ['GUILDS', 'GUILD_MESSAGES'],
+        defaultPrefix: ['|', 'Xnam!', 'xnam!'] 
+    }
+);
 client.login(process.env.DISCORD_TOKEN);
