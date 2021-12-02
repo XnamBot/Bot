e295c74b53fc66c51d7795c09289e15458096373
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-12-02 19:46:37 +0000

    added prefix's

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 const { SapphireClient, Command } = require('@sapphire/framework');
-const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'], defaultPrefix: ['|', 'Xnam!', 'xnam!'] });
 
 client.login(process.env.DISCORD_TOKEN);
