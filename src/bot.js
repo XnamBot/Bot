25c0b7d6e286a5c5344c8596e41c0b19e6f2fddd
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-12-24 14:28:32 +0000

    deleted everything again

--- src/bot.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const { SapphireClient, Command } = require('@sapphire/framework');
-const client = new SapphireClient(
-    {
-        intents: ['GUILDS', 'GUILD_MESSAGES'],
-        defaultPrefix: ['|', 'Xnam!', 'xnam!'] 
-    }
-);
-client.login(process.env.DISCORD_TOKEN);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./src/bot.js', { token: process.env.DISCORD_TOKEN });
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
