88202027bb026851392895d174be9717c853bb88
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-01-02 18:16:19 +0000

    image parts of the Animal commands

---
+++ src/commands/Animal/dog.ts
@@ -0,0 +1,13 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { SubCommandPluginCommand, SubCommandPluginCommandOptions } from '@sapphire/plugin-subcommands';
+import type { Message } from 'discord.js';
+import { AnimalApi } from '../../../lib/pets';
+
+@ApplyOptions<SubCommandPluginCommandOptions>({
+	description: 'Dog image'
+})
+export class UserCommand extends SubCommandPluginCommand {
+	public async messageRun(message: Message) {
+		message.channel.send(await AnimalApi(true, 'dog'));
+	}
+}
---
+++ src/commands/Animal/panda.ts
@@ -0,0 +1,13 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { SubCommandPluginCommand, SubCommandPluginCommandOptions } from '@sapphire/plugin-subcommands';
+import type { Message } from 'discord.js';
+import { AnimalApi } from '../../../lib/pets';
+
+@ApplyOptions<SubCommandPluginCommandOptions>({
+	description: 'Panda Images'
+})
+export class UserCommand extends SubCommandPluginCommand {
+	public async messageRun(message: Message) {
+		return message.channel.send(await AnimalApi(true, 'Panda'));
+	}
+}
