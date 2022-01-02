3e78f3bb62fc05f44589a34476d463eb2494f3c4
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-01-02 16:02:15 +0000

    updated the ping command to use the embed generator

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -2,18 +2,13 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Command, CommandOptions } from '@sapphire/framework';
 import { send } from '@sapphire/plugin-editable-commands';
 import type { Message } from 'discord.js';
-
+import { EmbedTemplate } from '../../lib/embeds';
 @ApplyOptions<CommandOptions>({
 	description: 'ping pong'
 })
 export class UserCommand extends Command {
 	public async messageRun(message: Message) {
 		const msg = await send(message, 'Ping?');
-
-		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
-			(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)
-		}ms.`;
-
-		return send(message, content);
+		message.channel.send({embeds: [EmbedTemplate('Ping', `Bot Latency: ${Math.round(this.container.client.ws.ping)}ms \n API Latency: ${(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)}ms`, 'Ping. Pong!')]})
 	}
 }
