5a25e88691694ffd866e031b5dada3aedd806d69
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-01-08 14:48:54 +0000

    Create credits.ts

---
+++ src/commands/General/credits.ts
@@ -0,0 +1,13 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { SubCommandPluginCommand, SubCommandPluginCommandOptions } from '@sapphire/plugin-subcommands';
+import type { Message } from 'discord.js';
+import { EmbedTemplate } from '../../lib/embeds';
+
+@ApplyOptions<SubCommandPluginCommandOptions>({
+	description: 'Bot credits'
+})
+export class UserCommand extends SubCommandPluginCommand {
+	public async messageRun(message: Message) {
+		message.channel.send({embeds: [EmbedTemplate('Credits', `**Owner**: \nSean Outram \nhttps://sean-outram.dev`, 'All those credits')]})
+	}
+}
