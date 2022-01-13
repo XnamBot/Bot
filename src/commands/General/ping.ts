e6b70f388adef14028e1bd7c2c057ea89803feb7
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-01-13 17:42:38 +0000

    added ping test methods

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -1,8 +1,9 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command, CommandOptions } from '@sapphire/framework';
+import { Command, CommandOptions, ApplicationCommandRegistry} from '@sapphire/framework';
 import { send } from '@sapphire/plugin-editable-commands';
-import type { Message } from 'discord.js';
+import type { CommandInteraction, Message } from 'discord.js';
 import { EmbedTemplate } from '../../lib/embeds';
+import { settings } from '../../config/config';
 @ApplyOptions<CommandOptions>({
 	description: 'Commands.General.Ping'
 })
@@ -10,5 +11,20 @@ export class UserCommand extends Command {
 	public async messageRun(message: Message) {
 		const msg = await send(message, 'Ping?');
 		message.channel.send({embeds: [EmbedTemplate('Ping', `Bot Latency: ${Math.round(this.container.client.ws.ping)}ms \n API Latency: ${(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)}ms`, 'Ping. Pong!')]})
+	}
+	public override async chatInputRun(interaction: CommandInteraction) {
+		interaction.reply('Test Success')
+	}
+	public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+		registry.registerChatInputCommand(
+			{
+				name: this.name,
+				description: 'Ping Test',
+			},
+			{
+				guildIds: [settings.devserverid],
+				idHints: ['']
+			}
+		);
 	}
 }
