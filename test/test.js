4df839289da51471f0065077d39fb4431de3ac01
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-01-21 09:50:30 +0000

    reversed testbyfire

--- test/test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs').promises;
-
-try {
-    const data = fs.readFile('../dist/');
-} catch (error) {
-    console.error(`Got an error trying to read the file: ${error.message}`);
-}
