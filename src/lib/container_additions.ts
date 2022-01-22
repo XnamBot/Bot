75afbbcca23dd1462b5a6e903f736d823d86dbed
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-01-22 07:33:41 +0000

    added the embedTemplate to the Container

--- src/lib/container_additions.ts
+++ src/lib/container_additions.ts
@@ -1,10 +1,13 @@
 import { container } from '@sapphire/framework';
 import { settings } from '../config/config';
+import { EmbedTemplate } from './embeds';
 
 container.config = settings;
+container.embed = EmbedTemplate;
 // Container Declarations
 declare module '@sapphire/pieces' {
 	interface Container {
 		config: object;
+		embed: any;
 	}
 }
