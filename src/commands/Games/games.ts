6f69d8dd0b3f96b917821e434eee5ea5342fcf4e
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-02-07 20:55:30 +0000

    Minesweep v.1.1

--- src/commands/Games/games.ts
+++ src/commands/Games/games.ts
@@ -4,7 +4,7 @@ import { XnamCommand } from '../../lib/structures/command';
 import { slashiesguilid } from '../../lib/slashiesguildid';
 import { slashiesidhint } from '../../lib/slashiesidhint';
 import { CommandInteraction, MessageActionRow, MessageButton } from 'discord.js';
-import { minesweeper_presetsfunction } from '../../lib/minesweeper';
+import { minesweeper_is_bomb, minesweeper_presetsfunction } from '../../lib/minesweeper';
 
 @ApplyOptions<CommandOptions>({
 	description: 'Commands.Games.Games'
@@ -16,7 +16,7 @@ export class GamesCommand extends XnamCommand {
 			const minesweeper_flag = this.container.emoji.flag;
 			const minesweeper_bomb = this.container.emoji.bomb;
 			var  minesweeper_gridarray = [this.container.emoji.one, this.container.emoji.two, this.container.emoji.three, this.container.emoji.four, this.container.emoji.five, this.container.emoji.six, this.container.emoji.seven, this.container.emoji.eight, this.container.emoji.nine]
-			var minesweeper_presetnum = Math.floor(Math.random() * 6)
+			var minesweeper_presetnum = Math.floor(Math.random() * 19)
 			const minesweeper_gamegridmap = minesweeper_presetsfunction(minesweeper_presetnum, minesweeper_flag, minesweeper_bomb);
 			const minesweeper_gamegrid = `${minesweeper_gridarray[0]}${minesweeper_gridarray[1]}${minesweeper_gridarray[2]}\n${minesweeper_gridarray[3]}${minesweeper_gridarray[4]}${minesweeper_gridarray[5]}\n${minesweeper_gridarray[6]}${minesweeper_gridarray[7]}${minesweeper_gridarray[8]}`
 			const minesweeper_buttonrow_one = new MessageActionRow()
@@ -76,54 +76,63 @@ export class GamesCommand extends XnamCommand {
 					const minesweeper_interaction_reply_new = {content: minesweeper_gamegrid_new, components: [minesweeper_buttonrow_one, minesweeper_buttonrow_two, minesweeper_buttonrow_three]}
 					i.reply({ content:`You got a ${minesweeper_gridarray[0]}`, ephemeral: true})
 					interaction.editReply(minesweeper_interaction_reply_new)
+					minesweeper_is_bomb(minesweeper_gridarray[0], interaction)
 				} else if (i.customId === 'games_minesweeper_two') {
 					minesweeper_gridarray[1] = minesweeper_gamegridmap[1]
 					const minesweeper_gamegrid_new = `${minesweeper_gridarray[0]}${minesweeper_gridarray[1]}${minesweeper_gridarray[2]}\n${minesweeper_gridarray[3]}${minesweeper_gridarray[4]}${minesweeper_gridarray[5]}\n${minesweeper_gridarray[6]}${minesweeper_gridarray[7]}${minesweeper_gridarray[8]}`
 					const minesweeper_interaction_reply_new = {content: minesweeper_gamegrid_new, components: [minesweeper_buttonrow_one, minesweeper_buttonrow_two, minesweeper_buttonrow_three]}
 					i.reply({ content:`You got a ${minesweeper_gridarray[1]}`, ephemeral: true})
 					interaction.editReply(minesweeper_interaction_reply_new)
+					minesweeper_is_bomb(minesweeper_gridarray[1], interaction)
 				} else if (i.customId === 'games_minesweeper_three') {
 					minesweeper_gridarray[2] = minesweeper_gamegridmap[2]
 					const minesweeper_gamegrid_new = `${minesweeper_gridarray[0]}${minesweeper_gridarray[1]}${minesweeper_gridarray[2]}\n${minesweeper_gridarray[3]}${minesweeper_gridarray[4]}${minesweeper_gridarray[5]}\n${minesweeper_gridarray[6]}${minesweeper_gridarray[7]}${minesweeper_gridarray[8]}`
 					const minesweeper_interaction_reply_new = {content: minesweeper_gamegrid_new, components: [minesweeper_buttonrow_one, minesweeper_buttonrow_two, minesweeper_buttonrow_three]}
 					i.reply({ content:`You got a ${minesweeper_gridarray[2]}`, ephemeral: true})
 					interaction.editReply(minesweeper_interaction_reply_new)
+					minesweeper_is_bomb(minesweeper_gridarray[2], interaction)
 				} else if (i.customId === 'games_minesweeper_four') {
 					minesweeper_gridarray[3] = minesweeper_gamegridmap[3]
 					const minesweeper_gamegrid_new = `${minesweeper_gridarray[0]}${minesweeper_gridarray[1]}${minesweeper_gridarray[2]}\n${minesweeper_gridarray[3]}${minesweeper_gridarray[4]}${minesweeper_gridarray[5]}\n${minesweeper_gridarray[6]}${minesweeper_gridarray[7]}${minesweeper_gridarray[8]}`
 					const minesweeper_interaction_reply_new = {content: minesweeper_gamegrid_new, components: [minesweeper_buttonrow_one, minesweeper_buttonrow_two, minesweeper_buttonrow_three]}
 					i.reply({ content:`You got a ${minesweeper_gridarray[3]}`, ephemeral: true})
 					interaction.editReply(minesweeper_interaction_reply_new)
+					minesweeper_is_bomb(minesweeper_gridarray[3], interaction)
 				} else if (i.customId === 'games_minesweeper_five') {
 					minesweeper_gridarray[4] = minesweeper_gamegridmap[4]
 					const minesweeper_gamegrid_new = `${minesweeper_gridarray[0]}${minesweeper_gridarray[1]}${minesweeper_gridarray[2]}\n${minesweeper_gridarray[3]}${minesweeper_gridarray[4]}${minesweeper_gridarray[5]}\n${minesweeper_gridarray[6]}${minesweeper_gridarray[7]}${minesweeper_gridarray[8]}`
 					const minesweeper_interaction_reply_new = {content: minesweeper_gamegrid_new, components: [minesweeper_buttonrow_one, minesweeper_buttonrow_two, minesweeper_buttonrow_three]}
 					i.reply({ content:`You got a ${minesweeper_gridarray[4]}`, ephemeral: true})
 					interaction.editReply(minesweeper_interaction_reply_new)
+					minesweeper_is_bomb(minesweeper_gridarray[4], interaction)
 				} else if (i.customId === 'games_minesweeper_six') {
 					minesweeper_gridarray[5] = minesweeper_gamegridmap[5]
 					const minesweeper_gamegrid_new = `${minesweeper_gridarray[0]}${minesweeper_gridarray[1]}${minesweeper_gridarray[2]}\n${minesweeper_gridarray[3]}${minesweeper_gridarray[4]}${minesweeper_gridarray[5]}\n${minesweeper_gridarray[6]}${minesweeper_gridarray[7]}${minesweeper_gridarray[8]}`
 					const minesweeper_interaction_reply_new = {content: minesweeper_gamegrid_new, components: [minesweeper_buttonrow_one, minesweeper_buttonrow_two, minesweeper_buttonrow_three]}
 					i.reply({ content:`You got a ${minesweeper_gridarray[5]}`, ephemeral: true})
 					interaction.editReply(minesweeper_interaction_reply_new)
+					minesweeper_is_bomb(minesweeper_gridarray[5], interaction)
 				} else if (i.customId === 'games_minesweeper_seven') {
 					minesweeper_gridarray[6] = minesweeper_gamegridmap[6]
 					const minesweeper_gamegrid_new = `${minesweeper_gridarray[0]}${minesweeper_gridarray[1]}${minesweeper_gridarray[2]}\n${minesweeper_gridarray[3]}${minesweeper_gridarray[4]}${minesweeper_gridarray[5]}\n${minesweeper_gridarray[6]}${minesweeper_gridarray[7]}${minesweeper_gridarray[8]}`
 					const minesweeper_interaction_reply_new = {content: minesweeper_gamegrid_new, components: [minesweeper_buttonrow_one, minesweeper_buttonrow_two, minesweeper_buttonrow_three]}
 					i.reply({ content:`You got a ${minesweeper_gridarray[6]}`, ephemeral: true})
 					interaction.editReply(minesweeper_interaction_reply_new)
+					minesweeper_is_bomb(minesweeper_gridarray[6], interaction)
 				} else if (i.customId === 'games_minesweeper_eight') {
 					minesweeper_gridarray[7] = minesweeper_gamegridmap[7]
 					const minesweeper_gamegrid_new = `${minesweeper_gridarray[0]}${minesweeper_gridarray[1]}${minesweeper_gridarray[2]}\n${minesweeper_gridarray[3]}${minesweeper_gridarray[4]}${minesweeper_gridarray[5]}\n${minesweeper_gridarray[6]}${minesweeper_gridarray[7]}${minesweeper_gridarray[8]}`
 					const minesweeper_interaction_reply_new = {content: minesweeper_gamegrid_new, components: [minesweeper_buttonrow_one, minesweeper_buttonrow_two, minesweeper_buttonrow_three]}
 					i.reply({ content:`You got a ${minesweeper_gridarray[7]}`, ephemeral: true})
 					interaction.editReply(minesweeper_interaction_reply_new)
+					minesweeper_is_bomb(minesweeper_gridarray[7], interaction)
 				} else if (i.customId === 'games_minesweeper_nine') {
 					minesweeper_gridarray[8] = minesweeper_gamegridmap[8]
 					const minesweeper_gamegrid_new = `${minesweeper_gridarray[0]}${minesweeper_gridarray[1]}${minesweeper_gridarray[2]}\n${minesweeper_gridarray[3]}${minesweeper_gridarray[4]}${minesweeper_gridarray[5]}\n${minesweeper_gridarray[6]}${minesweeper_gridarray[7]}${minesweeper_gridarray[8]}`
 					const minesweeper_interaction_reply_new = {content: minesweeper_gamegrid_new, components: [minesweeper_buttonrow_one, minesweeper_buttonrow_two, minesweeper_buttonrow_three]}
 					i.reply({ content:`You got a ${minesweeper_gridarray[8]}`, ephemeral: true})
 					interaction.editReply(minesweeper_interaction_reply_new)
+					minesweeper_is_bomb(minesweeper_gridarray[8], interaction)
 				}
 			});
 			collector?.on('end', collected => {
