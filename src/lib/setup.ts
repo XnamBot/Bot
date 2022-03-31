410f588238b0dcb82b901d224131b1bdf9443d47
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-03-31 19:19:52 +0100

    change to dotenv

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -6,13 +6,9 @@ import '@sapphire/plugin-logger/register';
 import '@sapphire/plugin-api/register';
 import '@kaname-png/plugin-statcord/register';
 import * as colorette from 'colorette';
-import { config } from 'dotenv-cra';
-import { join } from 'path';
 import { inspect } from 'util';
-import { srcDir } from './constants';
+import 'dotenv/config';
 require('./container_additions');
-// Read env var
-config({ path: join(srcDir, '.env') });
 
 // Set default inspection depth
 inspect.defaultOptions.depth = 1;
